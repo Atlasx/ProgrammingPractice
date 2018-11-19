7fa4a79cb22a3b36105c00eaf7538e0dd03e3e09
Connor Douglas <cjdouglas96@gmail.com>
2018-11-19 16:01:09 -0700

    Add starter templates

---
+++ template-shader/sketch.js
@@ -0,0 +1,12 @@
+function preload() {
+	//loadShader('./vert.glsl', './frag.glsl');
+	//shader(s);
+}
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
